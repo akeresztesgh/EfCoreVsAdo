9fea0f414df1b5f869a8ee7a6b8226e225d9fb1d
AJ Keresztes <aj@keresztes.us>
2022-05-12 18:22:26 -0400

    uncomments

--- efcorevsado/Benchmarks.cs
+++ efcorevsado/Benchmarks.cs
@@ -11,54 +11,54 @@ namespace efcorevsado
     [MemoryDiagnoser]
     public class Benchmarks
     {
-        //[Benchmark]
-        //[ArgumentsSource(nameof(SearchData))]
-        //public async Task EntityFrameworkSearch(List<long> lst)
-        //{
-        //    var da = new DataAccess();
-        //    await da.GetFromEf(lst);
-        //}
+        [Benchmark]
+        [ArgumentsSource(nameof(SearchData))]
+        public async Task EntityFrameworkSearch(List<long> lst)
+        {
+            var da = new DataAccess();
+            await da.GetFromEf(lst);
+        }
 
-        //[Benchmark]
-        //[ArgumentsSource(nameof(SearchData))]
-        //public async Task AdoSearch(List<long> lst)
-        //{
-        //    var da = new DataAccess();
-        //    await da.GetFromAdo(lst);
-        //}
+        [Benchmark]
+        [ArgumentsSource(nameof(SearchData))]
+        public async Task AdoSearch(List<long> lst)
+        {
+            var da = new DataAccess();
+            await da.GetFromAdo(lst);
+        }
 
-        //public IEnumerable<object> SearchData()
-        //{
-        //    yield return Enumerable.Range(0, 1000).Select(x => Convert.ToInt64(x)).ToList();
-        //    yield return Enumerable.Range(1000, 1000).Select(x => Convert.ToInt64(x)).ToList();
-        //    yield return Enumerable.Range(2000, 1000).Select(x => Convert.ToInt64(x)).ToList();
-        //    yield return Enumerable.Range(3000, 1000).Select(x => Convert.ToInt64(x)).ToList();
-        //    yield return Enumerable.Range(4000, 1000).Select(x => Convert.ToInt64(x)).ToList();
-        //}
+        public IEnumerable<object> SearchData()
+        {
+            yield return Enumerable.Range(0, 1000).Select(x => Convert.ToInt64(x)).ToList();
+            yield return Enumerable.Range(1000, 1000).Select(x => Convert.ToInt64(x)).ToList();
+            yield return Enumerable.Range(2000, 1000).Select(x => Convert.ToInt64(x)).ToList();
+            yield return Enumerable.Range(3000, 1000).Select(x => Convert.ToInt64(x)).ToList();
+            yield return Enumerable.Range(4000, 1000).Select(x => Convert.ToInt64(x)).ToList();
+        }
 
-        //[Benchmark]
-        //[ArgumentsSource(nameof(InsertData))]
-        //public async Task EntityFrameworkInsert(string name)
-        //{
-        //    var da = new DataAccess();
-        //    await da.InsertEf(name);
-        //}
+        [Benchmark]
+        [ArgumentsSource(nameof(InsertData))]
+        public async Task EntityFrameworkInsert(string name)
+        {
+            var da = new DataAccess();
+            await da.InsertEf(name);
+        }
 
-        //[Benchmark]
-        //[ArgumentsSource(nameof(InsertData))]
-        //public async Task AdoFrameworkInsert(string name)
-        //{
-        //    var da = new DataAccess();
-        //    await da.InsertAdo(name);
-        //}
+        [Benchmark]
+        [ArgumentsSource(nameof(InsertData))]
+        public async Task AdoFrameworkInsert(string name)
+        {
+            var da = new DataAccess();
+            await da.InsertAdo(name);
+        }
 
-        //public IEnumerable<object> InsertData()
-        //{
-        //    yield return "test1";
-        //    yield return "test2";
-        //    yield return "test3";
-        //    yield return "test4";
-        //}
+        public IEnumerable<object> InsertData()
+        {
+            yield return "test1";
+            yield return "test2";
+            yield return "test3";
+            yield return "test4";
+        }
 
         [Benchmark]
         [ArgumentsSource(nameof(InsertMultipleData))]
